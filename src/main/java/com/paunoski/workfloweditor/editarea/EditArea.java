9437dbe540a07113e92e8fd80e1c75d0f141bb4d
mpnsk <manuel.paunoski@gmail.com>
2017-10-07 22:02:41 +0200

    Soft refactor

--- src/main/java/com/paunoski/workfloweditor/editarea/EditArea.java
+++ src/main/java/com/paunoski/workfloweditor/editarea/EditArea.java
@@ -2,16 +2,13 @@ package com.paunoski.workfloweditor.editarea;
 
 import com.paunoski.workfloweditor.Place;
 import com.paunoski.workfloweditor.Transition;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Screen;
 
 public class EditArea {
     @FXML
@@ -28,41 +25,36 @@ public class EditArea {
         transition.relocate(250, 75);
         Node draggableTransition = transition.makeDraggable(transition);
 
-        Line line = new Line();
-        line.startXProperty().bind(place.layoutXProperty().add(place.translateXProperty().add(place.getRadius())));
-        line.startYProperty().bind(place.layoutYProperty().add(place.translateYProperty()));
-        line.endXProperty().bind(transition.layoutXProperty().add(transition.translateXProperty()));
-        line.endYProperty().bind(transition.layoutYProperty().add(transition.translateYProperty().add(transition.getHeight() / 2)));
 
+        EventHandler<MouseEvent> startDragging = click -> {
+            if (click.getButton().equals(MouseButton.PRIMARY)
+//                    && click.getClickCount() == 2)
+                    ) {
 
-        place.setOnMouseClicked(click -> {
-            if (click.getButton().equals(MouseButton.PRIMARY)) {
-                System.out.println("primary");
-                if (click.getClickCount() == 2) {
-                    System.out.println("double");
-
-                    Line mouseLine = new Line();
-                    mouseLine.startXProperty().bind(place.layoutXProperty().add(place.translateXProperty()));
-                    mouseLine.startYProperty().bind(place.layoutYProperty().add(place.translateYProperty()));
-                    mouseLine.setEndX(click.getSceneX());
-                    mouseLine.setEndY(click.getSceneY());
-                    root.getChildren().add(mouseLine);
-                    root.setOnMouseMoved(move -> {
-                        mouseLine.setEndX(move.getSceneX());
-                        mouseLine.setEndY(move.getSceneY());
-                    });
-                    System.out.println("setting up click listener");
-                    transition.setOnMouseClicked(event -> {
-                        if (event.getButton().equals(MouseButton.PRIMARY)) {
-                            System.out.println(event.getClickCount());
-                            root.setOnMouseMoved(null);
-                            mouseLine.endXProperty().bind(transition.layoutXProperty().add(transition.translateXProperty()));
-                            mouseLine.endYProperty().bind(transition.layoutYProperty().add(transition.translateYProperty()));
-                        }
-                    });
-                }
+                Line mouseLine = new Line();
+                mouseLine.startXProperty().bind(place.layoutXProperty().add(place.translateXProperty()));
+                mouseLine.startYProperty().bind(place.layoutYProperty().add(place.translateYProperty()));
+                mouseLine.setEndX(click.getSceneX());
+                mouseLine.setEndY(click.getSceneY());
+                root.getChildren().add(mouseLine);
+                EventHandler<MouseEvent> traceMouse = move -> {
+                    mouseLine.setEndX(move.getSceneX());
+                    mouseLine.setEndY(move.getSceneY());
+                };
+                root.setOnMouseMoved(traceMouse);
+                System.out.println("setting up click listener");
+                transition.setOnMouseClicked(event -> {
+                    if (event.getButton().equals(MouseButton.PRIMARY)) {
+                        System.out.println(event.getClickCount());
+                        root.setOnMouseMoved(null);
+                        mouseLine.endXProperty().bind(transition.layoutXProperty().add(transition.translateXProperty()));
+                        mouseLine.endYProperty().bind(transition.layoutYProperty().add(transition.translateYProperty()));
+                    }
+                });
             }
-        });
+
+        };
+        place.addEventHandler(MouseEvent.MOUSE_CLICKED, startDragging);
 
         root.getChildren().addAll(draggablePlace, draggableTransition);
     }
